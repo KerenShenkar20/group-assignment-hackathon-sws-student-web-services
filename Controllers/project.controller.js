7e8242ecd411e887964aaa9ec2de31270cadec3c
Itamar Yarden <itamary9@gmail.com>
2020-12-18 12:56:20 +0200

    itamar finished

---
+++ Controllers/project.controller.js
@@ -0,0 +1,44 @@
+const { query } = require('express');
+const { get } = require('http');
+const Project = require('./Models/project');
+
+let projectId = 100;
+let data;
+
+exports.projectDBController = {
+
+    getProjects(req, res) {
+
+
+        if (req.query) {
+
+            const query = {
+                programmingLanguage: `${req.body.programmingLanguage}`,
+                programmingLevel: `${req.body.programmingLevel}`,
+                role: `${req.body.role}`
+            }
+
+            Project.find(query)
+                .then(docs => { res.json(docs) })
+                .catch(err => console.log(`Error getting the data from DB: ${err}`));
+        }
+
+    },
+
+    createProject(req, res) {
+        ++projectId;
+
+        const project = new Project({ 
+            "id": projectId, 
+            "name":req.body.name, 
+            "volunteersId": req.body,volunteersId, 
+            "password": req.body.password, 
+            "description": req.body.description
+    })
+
+
+        project.save()
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    }
+}
